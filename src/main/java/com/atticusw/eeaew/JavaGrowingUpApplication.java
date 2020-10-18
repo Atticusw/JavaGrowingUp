483fa2a6cb22b06a2a7657563d3454434e427990
Atticuswx <jijie_king@163.com>
2020-10-18 21:53:18 +0800

    first commit

---
+++ src/main/java/com/atticusw/eeaew/JavaGrowingUpApplication.java
@@ -0,0 +1,13 @@
+package com.atticusw.eeaew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGrowingUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGrowingUpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/atticusw/eeaew/JavaGrowingUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atticusw.eeaew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGrowingUpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
